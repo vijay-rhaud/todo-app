a306806fba356d4be15ecde99ceb77a34e817f25
vijayr6 <vijayrhaud@gmail.com>
2020-08-19 23:32:08 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Todo Application
-This is my Todo Application that I built in Vue.js, using the Vue CLI
+
+> A todo application based in Vue.js
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="d-flex">
+    <div class="header">
+      <h1>Todo Application</h1>
+    </div>
+    <div class="d-flex user-input-section ">
+      <label for="todo"><p>Enter something to do!</p></label>
+      <input type="text" class="user-input" v-model="userInput">
+      <button class="button" @click="pushUserInput">Add to List</button>
+    </div>
+    <div class="todo-list">
+      <ol>
+        <li v-for="items in todoList" :key="items"><p>{{ items }}</p></li>
+      </ol>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        todoList: ['Get Money', 'Get Paid', 'Clean Room'],
+        userInput: '',
+        maxTasks: 10,
+      }
+    },
+    methods: {
+      pushUserInput() {
+        if(this.todoList.length >= this.maxTasks) {
+            return alert("You're not capable of doing more than 10 tasks at a time!");
+        }
+        this.todoList.push(this.userInput);
+        this.userInput = '';
+      }
+    }
+  }
+</script>
+<style>
+/** Global Styles **/
+h1, h2, h3, h4, h5, h6, p, ol, li, a {
+  font-family: 'Poppins', 'sans-serif'
+}
+.d-flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+.button {
+  margin-top: 24px;
+  background-color: #023e7d;
+  border: 0px none transparent;
+  border-radius: 8px;
+  color: #fff;
+  width: 240px;
+  height: 40px;
+  font-family: 'Poppins', 'sans-serif';
+  font-size: 16px;
+}
+/** Header Section Styles **/
+.header {
+  font-size: 24px;
+  color: #0353a4;
+  text-transform: uppercase;
+}
+/** User Input Section Styles **/
+.d-flex .user-input-section {
+  margin-bottom: 40px;
+}
+.user-input {
+  width: 400px;
+  height: 24px;
+}
+/** Todo List Section Styles **/
+.todo-list {
+  color: #023e7d;
+}
+</style>
