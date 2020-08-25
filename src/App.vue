8f146a71e8d2924a25ae0d49196fe054ee609b04
vijayr6 <vijayrhaud@gmail.com>
2020-08-25 19:35:23 +0100

    fix checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="todo-app d-flex">
-
     <app-header></app-header>
-
     <app-user-input @addNewTaskToList="pushUserInput"></app-user-input>
-
-    <app-todo-list :todoList="todoList" @deleteTask="deleteTaskFromArray"></app-todo-list>
-
+    <app-todo-list 
+      :todoList="todoList"
+      @updateTask="updateTaskInArray"
+      @deleteTask="deleteTaskFromArray"
+      @completeTask="completeTaskInArray"
+    >
+    </app-todo-list>
   </div>
 </template>
 <script>
   import Header from './components/Header.vue';
   import UserInput from './components/UserInput.vue';
   import TodoList from './components/TodoList.vue';
-
   export default {
     data() {
       return {
         todoList: [
-          {description: 'Learn Vue.js', isComplete: false},
-          {description: 'Make Todo App', isComplete: false},
-          {description: 'Learn Laravel', isComplete: false}
+          {description: 'Make a Todo List Application', isComplete: false},
         ],
         maxTasks: 10,
       }
@@ -28,15 +27,18 @@
     methods: {
       pushUserInput(newTask) {
         if (this.todoList.length >= this.maxTasks) {
-            return alert("You're not able to do more than 10 tasks at a time!");
+          return alert("You're not able to do more than 10 tasks at a time!");
         }
         this.todoList.push({description: newTask, isComplete: false});
       },
+      updateTaskInArray(index, task) {
+        this.todoList.splice(index, 1, {description: event.target.value, isComplete: false});
+      },
+      completeTaskInArray(index, task) {
+        this.todoList[index].isComplete = !this.todoList[index].isComplete
+      },      
       deleteTaskFromArray(index) {
         this.todoList.splice(index, 1);
-      },
-      test() {
-        console.log(this.todoList)
       }
     },
     components: {
@@ -47,33 +49,5 @@
   }
 </script>
 <style>
-/** Global Styles **/
-h1, h2, h3, h4, h5, h6, p, ol, li, a {
-  font-family: 'Poppins', 'sans-serif'
-}
-.d-flex {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-}
-.todo-app {
-  border: 8px solid #0353a4;
-  border-radius: 25px;
-  width: 60%;
-  margin: 120px auto;
-  padding-bottom: 120px;
-}
-.button {
-  border: 0 none transparent;
-  padding: 8px 12px;
-  color: #fff;
-  cursor: pointer;
-  font-size: 16px;
-  border-radius: 4px;
-}
-.button:hover {
-  transition: 0.1s;
-}
+  @import './assets/styles/global.css';
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,70 +1,34 @@
 <template>
     <div class="list-items d-flex">
-
-        <!-- <ol style="border-bottom: 1px solid red;">
-            <li v-for="items in todoList" :key="items"><p>{{ items }}</p></li>
-        </ol> -->
-
-        <!--- 
-            REMEMBER THE FOLLOWING:
-                . $event.target.value
-                . $emit
-                . @change
-                . @input 
-        -->
-
-        <!-- <div class="list-item" v-for="(items, index) in todoList" :key="items">
-
+        <div class="list-item" v-for="(task, index) in todoList" :key="index">
             <div class="list-input-section">
-
                 <input
-                    class="list-checkbox"                
+                    class="list-checkbox"
                     type="checkbox"
-                    @click="taskCompleted = !taskCompleted"
+                    @click="completeTask(index, task)"
                 >
-
                 <input
                     class="list-input"
-                    :class="completedTask()"
+                    :class="{'task-is-completed': task.isComplete}"
                     type="text"
                     :id="index"
-                    :value="items"
+                    :value="task.description"
                     :disabled="activeIndex !== index"
-                    @keyup.enter="updateTask(items, index)"
+                    @input="updateTask(index, task)"
                 >
-
             </div>
-            
             <div class="list-button-section">
-
-                <button class="button edit" @click="editTask(index);"><i class="fas fa-edit"></i></button>
-                <button class="button save" @click="saveTask()"><i class="fas fa-edit"></i></button>
-                <button class="button delete" @click="deleteTask(index)"><i class="fas fa-times"></i></button>
-
+                <button class="button edit" @click="editTask(index);">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <button class="button save" @click="saveTask()">
+                    <i class="fas fa-save"></i>
+                </button>
+                <button class="button delete" @click="deleteTask(index)">
+                    <i class="fas fa-times"></i>
+                </button>
             </div>
-
-        </div> -->
-        
-
-                <!-- :class="{red: taskCompleted} " -->
-
-        <ol >
-            <li 
-                v-for="(task, index) in todoList" :key="index"
-                :class="{red: taskCompleted} "                
-            >
-
-                {{task.description}}
-                -----------------
-                {{task.isComplete}}
-                -----------------
-                <button @click="completeTask(task, index)">Complete Task</button>
-
-            </li>
-        </ol>
-
-
-
+        </div>
     </div>
 </template>
 
@@ -81,99 +45,22 @@ export default {
         editTask(index) {
             this.activeIndex = index;
         },
-        updateTask(items, index) {                  
-            this.todoList.splice(index, 1, event.target.value);
-            this.activeIndex = '';
-        },
-        deleteTask(index) {
-            this.$emit('deleteTask', index);            
+        updateTask(index, task) {
+            this.$emit('updateTask', index, task);
         },
         saveTask() {
             this.activeIndex = ''
         },
-        completedTask() {
-            return {
-                'taskCompleted' : this.taskCompleted
-            }
+        completeTask(index, task) {
+            this.$emit('completeTask', index, task);
         },
-
-
-
-
-
-
-        completeTask(task, index) {
-            task.isComplete = !task.isComplete;
-            console.log(index);
-            console.log(task.description);
-            console.log(task.isComplete);
+        deleteTask(index) {
+            this.$emit('deleteTask', index);
         }
-
-
-
-
-
-
-
     }
 }
 </script>
 
 <style scoped>
-.list-items {
-    padding-bottom: 80px;
-}
-.list-item {
-    display: flex;    
-    width: 100%;
-    margin-bottom: 32px;
-}
-.list-input-section {
-    display: flex; 
-    justify-content: space-evenly;
-    align-items: center;
-    padding-left: 20px;
-    width: 100%; 
-    max-width: 70%; 
-}
-.list-input {
-    width: 100%;
-    max-width: 90%;
-    font-size: 20px;
-    font-family: 'Poppins', 'sans-serif';
-    padding-left: 12px;
-}
-.task-is-completed {
-    text-decoration: line-through;
-}
-.list-button-section {
-    width: 30%;
-    display: flex;
-    justify-content: space-evenly; 
-    padding-right: 20px;
-}
-.edit {
-    background-color: #28a745;
-}
-.edit:hover {
-    background-color: #218838;
-}
-.save {
-    background-color: #ffc107;
-}
-.save:hover {
-    background-color: #e0a800;
-}
-.delete {
-    background-color: #dc3545;
-}
-.delete:hover {
-    background-color: #c82333;
-}
-
-
-.red {
-    background-color: red;
-}
-
+  @import '../assets/styles/todo-list.css';
 </style>
