a65a3b3a022b447cb716c3dcaa6cb48bd1d04ee2
vijayr6 <vijayrhaud@gmail.com>
2020-08-26 19:07:11 +0100

    move header css into it's own file

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,15 +5,5 @@
 </template>
 
 <style scoped>
-.header {
-  width: 100%;
-  margin: 40px 0 48px 0;
-}
-.header h1{
-  text-align: center;
-  font-size: 40px;
-  color: #0353a4;
-  text-transform: uppercase;
-  margin: 0;
-}
+  @import '../assets/styles/header.css';
 </style>
